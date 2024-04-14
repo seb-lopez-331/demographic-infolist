b3b1ba5365156055c30f7aa5260be710a5695e9c
Sebastian Lopez <seb.lopez331@gmail.com>
2024-04-14 14:32:41 -0400

    feat: added a designated page for each address

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -2,4 +2,8 @@ class AddressesController < ApplicationController
     def index
         @addresses = Address.all
     end
+
+    def show
+        @address = Address.find(params[:id])
+    end
 end
