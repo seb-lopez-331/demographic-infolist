68f61ebb6f68772c51432f6d1fd5f09e5a376a40
Sebastian Lopez <seb.lopez331@gmail.com>
2024-04-14 22:59:20 -0400

    feat: add median income and household size

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -7,7 +7,7 @@ class AddressesController < ApplicationController
     ZIPCODE_INDEX = 3
 
     # Utility functions
-    def get_subpopulation(url)
+    def get_info(url)
         """
         The response body will look something like this
         [
@@ -29,17 +29,23 @@ class AddressesController < ApplicationController
         return parsed[1][0].to_i # This returns the subpopulation
     end
 
-    def get_educational_attainment(zipcode)
-        groups = ["S2301_C01_031E", "S2301_C01_032E", "S2301_C01_033E", "S2301_C01_034E", "S2301_C01_035E"]
+    def get_subpopulations(zipcode, groups)
         subpopulations = []
 
         groups.each do |group|
-            url = URI.parse('https://api.census.gov/data/2022/acs/acs5/subject?get=%s&for=zip+code+tabulation+area:%s' % [group, @zipcode])
-            subpopulations.push(get_subpopulation(url))
+            url = URI.parse('https://api.census.gov/data/2022/acs/acs5/subject?get=%s&for=zip+code+tabulation+area:%s' % [group, zipcode])
+            subpopulations.push(get_info(url))
         end
 
         return subpopulations
     end
+
+    def get_median_income(zipcode)
+        group = "S1901_C01_012E"
+        url = URI.parse('https://api.census.gov/data/2022/acs/acs5/subject?get=%s&for=zip+code+tabulation+area:%s' % [group, zipcode])
+        return get_info(url)
+    end
+
 
     # Controller functions
     def index
@@ -49,27 +55,35 @@ class AddressesController < ApplicationController
     def show
         @address = Address.find(params[:id])
         
-        # Do a &for=zip+code+tabulation_area:ZIPCODE
-        # And a &for=state:STATECODE (eg NY)
-        # And a &key=APIKEY
+        # Gather fields for displaying purposes
         fields = @address.address.split(",")
         @city = fields[CITY_INDEX]
         @state = fields[STATE_INDEX]
         @zipcode = fields[ZIPCODE_INDEX]
         
-        educational_subpopulations = get_educational_attainment(@zipcode)
-        @total = educational_subpopulations[0]
+        # Gather subpopulations for each each educational attainment
+        educational_subpopulations = get_subpopulations(@zipcode, ["S2301_C01_031E", "S2301_C01_032E", "S2301_C01_033E", "S2301_C01_034E", "S2301_C01_035E"])
+        @total_educated = educational_subpopulations[0]
         @under_hs = educational_subpopulations[1]
         @hs_grad = educational_subpopulations[2]
         @some_college = educational_subpopulations[3]
         @bachelors = educational_subpopulations[4]
 
+        # Gather median household income
+        @median_income = get_median_income(@zipcode)
+
+        # Gather subpopulations for each houshold size
+        household_size_subpopulations = get_subpopulations(@zipcode, ["S2501_C01_001E", "S2501_C01_002E", "S2501_C01_003E", "S2501_C01_004E", "S2501_C01_005E"])
+        @occupied_units = household_size_subpopulations[0]
+        @one_person = household_size_subpopulations[1]
+        @two_people = household_size_subpopulations[2]
+        @three_people = household_size_subpopulations[3]
+        @four_or_more = household_size_subpopulations[4]
 
         # We can show 
             # Educational attainment
             # Median income levels
-            # Housing quality
-            # Median household size
+            # Household size
 
         """
         Procedure:
